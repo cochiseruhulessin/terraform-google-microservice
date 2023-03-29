949cec5c9d85e80c46f1b4256d63b3bcdc163446
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2023-03-29 13:50:40 +0200

    Add storage module

---
+++ modules/storage/main.tf
@@ -0,0 +1,25 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+variable "bucket_name" { type = string }
+variable "bucket_location" { type = string}
+variable "project" { type = string }
+
+
+resource "google_storage_bucket" "app" {
+  force_destroy               = true
+  location                    = var.bucket_location
+  name                        = var.bucket_name
+  project                     = var.project
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.app.name
+}
